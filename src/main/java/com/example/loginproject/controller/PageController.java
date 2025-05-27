8f865aa2b7803c18ef91ec445c0c17160b52cc08
uuzjin <lyj5721@sookmyung.ac.kr>
2025-05-27 19:06:59 +0900

    index.html 경로 수정

--- src/main/java/com/example/loginproject/controller/PageController.java
+++ src/main/java/com/example/loginproject/controller/PageController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PageController {
-    @GetMapping("/index")
-    public String index() {
-        return "index";
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/index.html";
     }
 
     @GetMapping("/login")
