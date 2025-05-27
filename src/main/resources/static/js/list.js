fb38271ba0ad7b76d9efc92b4f5739cb02ef936d
seoinjeong24 <seoseoin@sookmyung.ac.kr>
2025-05-28 01:01:42 +0900

    사이드바 추가

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -65,3 +65,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+//사이드바 접힘&펼침
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleBtn = document.querySelector(".sidebar-toggle");
+    const sidebar = document.querySelector("aside");
+
+    toggleBtn.addEventListener("click", () => {
+      sidebar.classList.toggle("active");
+    });
+  });
