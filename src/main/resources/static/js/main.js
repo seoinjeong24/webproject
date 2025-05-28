57ad62ae9aeef73f51d708fc4e633cbf55b123c1
임유진 <lyj5721@sookmyung.ac.kr>
2025-05-28 14:30:39 +0900

    목록 경로 이동 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,12 @@
 function navigateTo(path) {
+  const username = localStorage.getItem("username");
+  const userId = localStorage.getItem("userId");
+
+  if (!username || !userId) {
+    alert("로그인이 필요합니다.");
+    window.location.href = "login.html";
+    return;
+  }
+
   window.location.href = path;
 }
